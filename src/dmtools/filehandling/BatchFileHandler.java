79d1074c22f0dbd88136b48ffe853522576cc04f
artg.dev <artg3.dev@gmail.com>
2020-01-24 19:25:02 -0500

    Update BatchFileHandler.java
    added monster file capabilities

--- src/dmtools/filehandling/BatchFileHandler.java
+++ src/dmtools/filehandling/BatchFileHandler.java
@@ -5,6 +5,7 @@
  */
 package dmtools.filehandling;
 
+import dmtools.game.entities.Monster;
 import dmtools.game.entities.PC;
 import java.io.File;
 import java.io.FileFilter;
@@ -14,18 +15,27 @@ import java.util.ArrayList;
  *
  * @author A3
  */
+public class BatchFileHandler {
+
     public BatchFileHandler() {
     }
-    
-    public ArrayList<PC> loadAllPcs() throws Exception{
+
+    public ArrayList<PC> loadAllPcs() throws Exception {
         ArrayList<PC> pcs = new ArrayList();
         File pcDir = new File("User/PCs");
-        for (File i : pcDir.listFiles(this)) {
         for (File i : pcDir.listFiles(new DndFileFilter(DndFileFilter.PC))) {
             pcs.add((PC) FileHandler.loadFromFile(i));
         }
         return pcs;
     }
 
+    public ArrayList<Monster> loadAllMonsters() throws Exception {
+        ArrayList<Monster> monsters = new ArrayList();
+        File monDir = new File("User/Monsters");
+        for (File i : monDir.listFiles(
+                new DndFileFilter(DndFileFilter.MONSTER))) {
+            monsters.add((Monster) FileHandler.loadFromFile(i));
+        }
+        return monsters;
     }
 }
