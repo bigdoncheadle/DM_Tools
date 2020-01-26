270335937657f84a1f50c6696cfc28b3cc0d25a1
artg.dev <artg3.dev@gmail.com>
2020-01-26 16:16:01 -0500

    Update NonPlayerGetInitiativePanel.java
    changed workings of how entities are added to allow for the removeEntity method.

--- src/dmtools/GUI/initiativeguicomponents/createinitiative/panels/NonPlayerGetInitiativePanel.java
+++ src/dmtools/GUI/initiativeguicomponents/createinitiative/panels/NonPlayerGetInitiativePanel.java
@@ -10,6 +10,8 @@ import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import javax.swing.JLabel;
@@ -23,40 +25,70 @@ import javax.swing.JTextField;
 public class NonPlayerGetInitiativePanel extends JPanel {
 
     private Map<DNDEntity, JTextField> inputs;
-    private int workingGridY;
+    private Map<DNDEntity, JLabel> labels;
+    private ArrayList<DNDEntity> entities;
 
     public NonPlayerGetInitiativePanel() {
         super();
         inputs = new HashMap();
-        workingGridY = 1;
+        labels = new HashMap();
+        entities = new ArrayList();
         createComponents();
     }
 
-    public void add(DNDEntity e) {
+    public ArrayList<DNDEntity> getEntities() {
+        return entities;
+    }
+
+    public void addEntity(DNDEntity e) {
+        entities.add(e);
+        Collections.sort(entities);
+
+        int workingGridY = entities.indexOf(e) + 1;
         GridBagConstraints c;
 
         // Label
         JLabel name = new JLabel(e.getName());
-        c = new GridBagConstraints();
-        c.gridx = 0;
-        c.gridy = workingGridY;
-        c.weightx = 1;
-        c.anchor = GridBagConstraints.FIRST_LINE_END;
-        c.insets = new Insets(5, 5, 0, 5);
-        add(name, c);
+        labels.put(e, name);
 
         // TextField
         JTextField tf = new JTextField(2);
         inputs.put(e, tf);
-        
-        c.gridx = 1;
-        c.anchor = GridBagConstraints.LINE_START;
-        c.insets = new Insets(5, 0, 0, 5);
-        add(tf, c);
-        
-        workingGridY++;
+
+        updateEntityList();
         revalidate();
         repaint();
+    }
+
+    public void removeEntity(DNDEntity e) {
+        entities.remove(e);
+        remove(labels.get(e));
+        remove(inputs.get(e));
+        labels.remove(e);
+        inputs.remove(e);
+
+        revalidate();
+        repaint();
+    }
+
+    private void updateEntityList() {
+        for (DNDEntity i : entities) {
+
+            // Label
+            GridBagConstraints c = new GridBagConstraints();
+            c.gridx = 0;
+            c.gridy = entities.indexOf(i) + 1;
+            c.weightx = 1;
+            c.anchor = GridBagConstraints.FIRST_LINE_END;
+            c.insets = new Insets(5, 5, 0, 5);
+            add(labels.get(i), c);
+
+            // TextField
+            c.gridx = 1;
+            c.anchor = GridBagConstraints.LINE_START;
+            c.insets = new Insets(5, 0, 0, 5);
+            add(inputs.get(i), c);
+        }
     }
 
     private void createComponents() {
