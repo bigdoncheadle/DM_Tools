cfd5c533b0b2b13962a87b05c87202680dbf790a
artg.dev <artg3.dev@gmail.com>
2020-01-26 10:01:24 -0500

    Create MonsterComboBox.java
    lists all monsters saved in the monster folder of the user.

---
+++ src/dmtools/GUI/monsters/hordes/MonsterComboBox.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dmtools.GUI.monsters.hordes;
+
+import dmtools.filehandling.BatchFileHandler;
+import dmtools.game.entities.Monster;
+import javax.swing.JComboBox;
+
+/**
+ *
+ * @author A3
+ */
+public class MonsterComboBox extends JComboBox {
+
+    public MonsterComboBox() {
+        super();
+        fillBox();
+    }
+    
+    public void refresh() {
+        this.removeAllItems();
+        fillBox();
+    }
+
+    private void fillBox() {
+        BatchFileHandler batch = new BatchFileHandler();
+        this.addItem("-Select a Monster-");
+        for (String i : batch.loadAllFileNames(Monster.class)) {
+            this.addItem(i);
+        }
+        this.setMaximumRowCount(this.getItemCount());
+    }
+}
