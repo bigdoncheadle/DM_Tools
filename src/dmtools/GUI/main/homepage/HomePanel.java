6d326b72dc41565c92430b312378a957eb6f2496
artg.dev <artg3.dev@gmail.com>
2020-01-31 20:48:31 -0500

    Update HomePanel.java
    changed color of header font.
    
    still not happy

--- src/dmtools/GUI/main/homepage/HomePanel.java
+++ src/dmtools/GUI/main/homepage/HomePanel.java
@@ -48,7 +48,7 @@ public class HomePanel extends JPanel {
         // Header
         JLabel header = new JLabel(headerText, SwingConstants.CENTER);
         header.setFont(header.getFont().deriveFont(40f));
-        header.setForeground(LayoutConstants.YELLOW);
+        header.setForeground(LayoutConstants.GREEN);
         header.setAlignmentX(Component.CENTER_ALIGNMENT);
         c = new GridBagConstraints();
         c.gridx = 0;
