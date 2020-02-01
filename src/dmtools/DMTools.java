4b5b58128fc9ef26c8c9fb58ab2a6792fc51d66a
artg.dev <artg3.dev@gmail.com>
2020-02-01 09:00:00 -0500

    Update DMTools.java
    loads a testing party and then begins the main gui

--- src/dmtools/DMTools.java
+++ src/dmtools/DMTools.java
@@ -5,8 +5,11 @@
  */
 package dmtools;
 
-import dmtools.GUI.TestingGUI;
 import dmtools.GUI.main.DMToolsGui;
+import dmtools.filehandling.FileHandler;
+import dmtools.playermgmt.PlayerParty;
+import java.io.File;
+import java.io.IOException;
 import javax.swing.SwingUtilities;
 
 /**
@@ -14,15 +17,22 @@ import javax.swing.SwingUtilities;
  * @author A3
  */
 public class DMTools {
+
     private static final String VERSION = "v.0.1-alpha";
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        try {
+            PlayerParty party = (PlayerParty) FileHandler.loadFromName(
+                    "Test Party", FileHandler.PLAYER_PARTY_FILE);
+            DMToolsGui dmGui = new DMToolsGui(VERSION, party);
+            SwingUtilities.invokeLater(dmGui);
+        } catch (IOException e) {
+            System.out.println("oops");
+        }
 
-        TestingGUI gui = new TestingGUI();
-        DMToolsGui dmGui = new DMToolsGui(VERSION);
-        SwingUtilities.invokeLater(dmGui);
     }
 }
